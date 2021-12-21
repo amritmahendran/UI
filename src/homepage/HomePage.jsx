7020ca06b7c260d65e1e6ddfe40271c09a6f110f
liuzuolin1996 <liuzuolin1996@gmail.com>
2021-12-21 23:29:18 +0800

    added version1 code

---
+++ src/homepage/HomePage.jsx
@@ -0,0 +1,7 @@
+function HomePage() {
+    return <div>
+        This is the home page
+    </div>
+}
+
+export default HomePage
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+import Header from "./common/Header"
+import HomePage from "./homepage/HomePage"
+import AboutPage from "./aboutpage/AboutPage"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <BrowserRouter>
+
+    <Header/>
+    <Routes>
+
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/about" element={<AboutPage/>} />
+
+    </Routes>
+
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
